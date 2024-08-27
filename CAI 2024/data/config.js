757885fbc7d07bd476aaa8a79e8c80dc5d4df837
Yufan <e0773591@u.nus.edu>
2024-08-27 10:04:29 +0800

    Add CAI stuff inside

---
+++ CAI 2024/data/config.js
@@ -0,0 +1,18 @@
+var config = {
+	"proceeding": {
+		"acronym": "CAI",
+		"year": 2024
+	},
+	"bannerFile": "content/images/CAI 2024 Banner.jpg",
+	"enableGDPR": false,
+	"enableAffiliationIndex": false,
+	"videoPlayer": "mediaelement",
+	"webpubDownload": {
+		"enabled": null,
+		"location": "pdfs/CAI 2024.zip"
+	},
+	"services": {
+		"authEnabled": false,
+		"trackingEnabled": false
+	}
+};
